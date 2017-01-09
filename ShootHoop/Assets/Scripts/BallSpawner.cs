da2cbac5b3b1539b183506b1fc41c4fde8e762aa
Hannah <hannahblair@hotmail.co.uk>
2017-01-09 00:56:47 +0000

    ShootHoop VR Game Initial Commit

---
+++ ShootHoop/Assets/Scripts/BallSpawner.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallSpawner : MonoBehaviour {
+
+    public GameObject ballPrefab;
+
+	// Use this for initialization
+	void Start () {
+       
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        float velocity = 5.0f;
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            GameObject instance = Instantiate(ballPrefab);
+            Rigidbody rb = instance.GetComponent<Rigidbody>();
+            rb.velocity = Vector3.left * velocity;
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            GameObject instance = Instantiate(ballPrefab);
+            Rigidbody rb = instance.GetComponent<Rigidbody>();
+            rb.velocity = Vector3.right * velocity;
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            GameObject instance = Instantiate(ballPrefab);
+            Rigidbody rb = instance.GetComponent<Rigidbody>();
+            rb.velocity = Vector3.forward * velocity;
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            GameObject instance = Instantiate(ballPrefab);
+            Rigidbody rb = instance.GetComponent<Rigidbody>();
+            rb.velocity = Vector3.back * velocity;
+        }
+    }
+}
