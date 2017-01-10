17e77f90487892321e22d628536539a0e39119ea
Hannah <hannahblair@hotmail.co.uk>
2017-01-10 00:37:37 +0000

    Game update
    Scoring and camera rotation updated

---
+++ ShootHoop/Assets/Scripts/ScoreKeeper.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreKeeper : MonoBehaviour {
+
+    int score = 0;
+
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+
+
+    public void IncrementScore()
+    {
+        score++;
+        print("You scored! Your current score is: " + score);
+    }
+}
